632402a4174952ce153171ac24f73a3ff4faeecf
Jure-Z <82453768+Jure-Z@users.noreply.github.com>
2023-12-30 14:47:09 +0100

    added flashlight to player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -28,14 +28,18 @@ public class PlayerMovement : MonoBehaviour
             float horizontalInput = Input.GetAxisRaw("Horizontal");
             float verticalInput = Input.GetAxisRaw("Vertical");
 
-            // Calculate movement direction
+            //calculate horizontal rotation from camera rotation
             Quaternion camRotation = cameraObj.transform.rotation;
             float yaw = Mathf.Atan2(2*camRotation.y*camRotation.w - 2*camRotation.x*camRotation.z, 1 - 2*camRotation.y*camRotation.y - 2*camRotation.z*camRotation.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, yaw, 0);
+
+            // Calculate accelaration direction
             Vector3 movementDirectForward =  (Quaternion.Euler(0, yaw, 0) * Vector3.forward) * verticalInput;
             Vector3 movementDirectRight =  (Quaternion.Euler(0, yaw, 0) * Vector3.right) * horizontalInput;
 
             Vector3 accelaration = Vector3.Normalize(movementDirectForward + movementDirectRight) * accelarationSize;
 
+            //add accelaration or decelaration to velocity
             if(accelaration.magnitude == 0) {
                 velocity = Vector3.Normalize(velocity) * Mathf.Max(velocity.magnitude - decelarationSize * Time.deltaTime, 0);
             }
