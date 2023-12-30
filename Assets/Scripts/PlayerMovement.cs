ba19f16ea0b2f12b6ab56981c0b625352b662f60
Jure-Z <82453768+Jure-Z@users.noreply.github.com>
2023-12-30 12:57:14 +0100

    added accelaration to player movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,12 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     // Start is called before the first frame update
-    public float moveSpeed = 5f;
+    public float accelarationSize = 20f;
+    public float decelarationSize = 30f;
+    public float topSpeed = 6f;
     private bool canMove = true;
+
+    private Vector3 velocity = new Vector3(0,0,0);
 
     public GameObject cameraObj;
 
@@ -21,8 +25,8 @@ public class PlayerMovement : MonoBehaviour
         if (canMove)
         {
             // Get input for movement
-            float horizontalInput = Input.GetAxis("Horizontal");
-            float verticalInput = Input.GetAxis("Vertical");
+            float horizontalInput = Input.GetAxisRaw("Horizontal");
+            float verticalInput = Input.GetAxisRaw("Vertical");
 
             // Calculate movement direction
             Quaternion camRotation = cameraObj.transform.rotation;
@@ -30,10 +34,26 @@ public class PlayerMovement : MonoBehaviour
             Vector3 movementDirectForward =  (Quaternion.Euler(0, yaw, 0) * Vector3.forward) * verticalInput;
             Vector3 movementDirectRight =  (Quaternion.Euler(0, yaw, 0) * Vector3.right) * horizontalInput;
 
-            Vector3 movement = Vector3.Normalize(movementDirectForward + movementDirectRight) * moveSpeed * Time.deltaTime;
+            Vector3 accelaration = Vector3.Normalize(movementDirectForward + movementDirectRight) * accelarationSize;
+
+            if(accelaration.magnitude == 0) {
+                velocity = Vector3.Normalize(velocity) * Mathf.Max(velocity.magnitude - decelarationSize * Time.deltaTime, 0);
+            }
+            else {
+                velocity = velocity + accelaration * Time.deltaTime;
+                if(velocity.magnitude > topSpeed)
+                    velocity = Vector3.Normalize(velocity) * topSpeed;
+            }
 
             // Move the player
-            transform.Translate(movement);
+            CharacterController controller = GetComponent<CharacterController>();
+            controller.Move(velocity * Time.deltaTime);
+            //transform.Translate(movement);
         }
+    }
+
+    void OnControllerColliderHit(ControllerColliderHit col)
+    {
+        //Debug.Log(col.collider.gameObject);
     }
 }
